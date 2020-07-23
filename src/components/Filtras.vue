f56a5b05e3516f1b7e7d52e26726b4fd7cc80724
pakidaxed <jucius.tomas@gmail.com>
2020-07-23 21:44:08 +0300

    filtrus darom su js is api paimtu duomenu

---
+++ src/components/Filtras.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="filter">
+        <p>Filter:</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Filter",
+        data(){
+            return {
+                filters: [{
+                    vln: 'Vilnius',
+                    kns: 'Kaunas',
+                    klp: 'Klaipėda'
+                }]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="navigation">
+        <div id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Navigation"
+    }
+</script>
+
+<style scoped>
+
+</style>
